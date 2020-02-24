98a587c9b97c5b3e3d3e42cf6a300045972ae813
shreeganes <shreevidhya.ganesan@byon8.com>
2020-02-24 13:10:52 +0100

    modified port

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const counterRoutes = require('./routes/counter');
 
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use('/api', counterRoutes);
 
-app.listen(8080);
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server is yup on port 8080');
+});
